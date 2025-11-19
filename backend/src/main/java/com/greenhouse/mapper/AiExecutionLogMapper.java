07ab549e62aa03358b1694c4ef72b084cb66223a
hubaolong@lv-ju.com <hubaolong@lv-ju.com>
2025-11-19 10:26:08 +0800

    所有文件

---
+++ backend/src/main/java/com/greenhouse/mapper/AiExecutionLogMapper.java
@@ -0,0 +1,13 @@
+package com.greenhouse.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.greenhouse.entity.AiExecutionLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * AI执行日志 Mapper
+ */
+@Mapper
+public interface AiExecutionLogMapper extends BaseMapper<AiExecutionLog> {
+}
+
